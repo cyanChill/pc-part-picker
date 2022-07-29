a09bc6ad8f064aa1f2f43e81afad8b31f9e46635
cyanChill <anthonyliang9@gmail.com>
2022-07-29 12:58:31 -0400

    add expiration to items we cache with our handlers

--- public/sw-base.js
+++ public/sw-base.js
@@ -1,12 +1,25 @@
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
-const { ExpirationPlugin } = workbox.expiration;
+const { ExpirationPlugin, CacheExpiration } = workbox.expiration;
 const { precacheAndRoute, getCacheKeyForURL } = workbox.precaching;
 const { NavigationRoute, registerRoute } = workbox.routing;
 const { StaleWhileRevalidate, NetworkFirst } = workbox.strategies;
 
 const VERSION_NUMBER = "v1.0.0";
+
+// Set expiration time for all non-precached routes
+const PG_ROUTES_CACHE_NAME = "page-routes";
+const routeExpirationManager = new CacheExpiration(PG_ROUTES_CACHE_NAME, {
+  maxAgeSeconds: 60 * 60 * 24 * 3,
+});
+
+// Set expiration time for all non-precached images
+const DYNAMIC_IMGS_CACHE_NAME = "images";
+const imgExpirationManager = new CacheExpiration(DYNAMIC_IMGS_CACHE_NAME, {
+  maxAgeSeconds: 60 * 60 * 24 * 3,
+  maxEntries: 20,
+});
 
 /* 
   -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -32,40 +45,34 @@ registerRoute(
     url.origin === self.location.origin &&
     event.request.headers.get("accept").includes("text/html"),
 
-  ({ event, url }) => {
-    // Network-First
-    return fetch(event.request)
-      .then((res) => {
-        return caches.open("page-routes").then((cache) => {
-          // Save Network response in cache
-          cache.put(event.request.url, res.clone());
-          return res; // Return network response
-        });
-      })
-      .catch((err) => {
-        if (url.pathname === "/") {
-          // Look to see if there exists a dynamically cached version of
-          // the homepage before serving the one from the precache
-          return caches.open("page-routes").then((cache) => {
-            return cache.match(event.request).then((res) => {
-              if (res) return res; // Return dynamic cache result
-              return caches.match(getCacheKeyForURL("/")); // Return precache result
-            });
-          });
-        } else {
-          // Return cached response (for all other routes)
-          return caches
-            .match(event.request)
-            .then((res) => {
-              if (res) return res;
-              throw new Error("Page Not Found in Cache.");
-            })
-            .catch((err) => {
-              // Render Offline Fallback Page
-              return caches.match(getCacheKeyForURL("/offline"));
-            });
-        }
-      });
+  async ({ request, url }) => {
+    const pgRouteCache = await caches.open(PG_ROUTES_CACHE_NAME);
+    // ⭐ Network-First Implementation ⭐
+    try {
+      const res = await fetch(request);
+      // Save network response in cache
+      await pgRouteCache.put(request.url, res.clone());
+      // Update cache expiration time for result
+      await routeExpirationManager.updateTimestamp(request.url);
+      await routeExpirationManager.expireEntries(); // Clean up cache
+
+      return res; // Return fetched result
+    } catch (err) {
+      // Error thrown if offline
+      if (url.pathname === "/") {
+        // Look to see if there exists a dynamically cached version of
+        // the homepage before serving the one from the precache
+        const res = await pgRouteCache.match(request);
+        if (res) return res; // Return dynamic cache result
+        return caches.match(getCacheKeyForURL("/")); // Return precache result
+      } else {
+        // Return cached response (for all other routes)
+        const cachedRes = await caches.match(request);
+        if (cachedRes) return cachedRes;
+        // Render Offline Fallback Page
+        return caches.match(getCacheKeyForURL("/offline"));
+      }
+    }
   },
   "GET"
 );
@@ -81,21 +88,20 @@ const POST_ROUTES_REGEX = [
   /(brands|builds|category|products)\/.*\/(add|update|delete|validateSavePass)/,
 ];
 registerRoute(
-  ({ event, url }) =>
+  ({ url }) =>
     url.origin === self.location.origin &&
     POST_ROUTES_REGEX.some((rx) => rx.test(url.pathname)),
 
-  ({ event }) => {
-    // Network-Only
-    return fetch(event.request)
-      .then((res) => {
-        if (res) return res;
-        throw new Error("Action Unsupported Offline.");
-      }) // Return response fetched
-      .catch((err) => {
-        // Render Unsupported Action Fallback Page
-        return caches.match(getCacheKeyForURL("/unsupported"));
-      });
+  async ({ request }) => {
+    // ⭐ Network-Only Implementation ⭐
+    try {
+      const res = await fetch(request);
+      if (res) return res; // Return response fetched
+      throw new Error("Action Unsupported Offline.");
+    } catch (err) {
+      // Render Unsupported Action Fallback Page
+      return caches.match(getCacheKeyForURL("/unsupported"));
+    }
   },
   "POST"
 );
@@ -121,17 +127,17 @@ precacheAndRoute(
           Cache-First Strategy For All Other Images
   -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 */
-registerRoute(/.*\.(png|jpg|webp)|/, ({ event }) => {
+registerRoute(/.*\.(png|jpg|webp)|/, async ({ request }) => {
   // Look for image in cache
-  return caches.match(event.request).then((res) => {
-    if (res) return res;
-    // If doesn't exist in the cache, fetch the result
-    return fetch(event.request).then((res) => {
-      return caches.open("images").then((cache) => {
-        // Save Network response in cache
-        cache.put(event.request.url, res.clone());
-        return res; // Return network response
-      });
-    });
-  });
+  const cacheResult = await caches.match(request);
+  if (cacheResult) return cacheResult;
+  // If it doesn't exist in cache, fetch the result
+  const fetchResult = await fetch(request);
+  const dynamicImgCache = await caches.open(DYNAMIC_IMGS_CACHE_NAME);
+  // Save Network response in cache
+  await dynamicImgCache.put(request.url, fetchResult.clone());
+  await imgExpirationManager.updateTimestamp(request.url);
+  await imgExpirationManager.expireEntries(); // Clean up cache
+
+  return fetchResult; // Return network response
 });
